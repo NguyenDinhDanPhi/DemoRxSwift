63a10887ca7e4fc73cb1849cbb19cb66ddd4ccf5
danphi <phittnh@gmail.com>
2023-03-13 14:01:33 +0700

    add

--- DemoRxSwift/MyPlayground.playground/Contents.swift
+++ DemoRxSwift/MyPlayground.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
+import RxSwift
+import RxRelay
+// 1
+enum MyError: Error {
+    case anError
+}
 
-var greeting = "Hello, playground"
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+  print(label, (event.element ?? event.error) ?? event)
+}
+
+let relay = BehaviorRelay(value: "Initial value")
+ let disposeBag = DisposeBag()
+
+ // 2
+ relay.accept("New initial value")
+relay
+    .subscribe {
+      print(label: "1)", event: $0)
+    }
+    .disposed(by: disposeBag)
+// 1
+relay.accept("1")
+
+// 2
+relay
+  .subscribe {
+    print(label: "2)", event: $0)
+  }
+  .disposed(by: disposeBag)
+
+// 3
+relay.accept("2")
+relay.accept("3")
+print(relay.value)
--- DemoRxSwift/ViewController.swift
+++ DemoRxSwift/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+struct Product {
+    let imageName: String
+    let tittle: String
+}
+struct ProductViewModel {
+    var item = PublishSubject<[Product]>()
+    func fetchItem() {
+        let products = [
+            Product(imageName: "house", tittle: "Home"),
+            Product(imageName: "gear", tittle: "Setting"),
+            Product(imageName: "person.cirle", tittle: "Profile"),
+            Product(imageName: "airplane", tittle: "Flights"),
+            Product(imageName: "bell", tittle: "Activity")
+        
+        ]
+        item.onNext(products)
+        item.onCompleted()
+    }
+}
 
 class ViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var viewModel = ProductViewModel()
+    private var bag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        bindTableData()
     }
 
+    func bindTableData() {
+        // bind item to table
+        viewModel.item.bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) { row, item,cell in
+            cell.textLabel?.text = item.tittle
+            cell.imageView?.image = UIImage(systemName: item.imageName)
+        }.disposed(by: bag)
+        //bind a model selected handle
+        tableView.rx.modelSelected(Product.self).bind {
+            product in
+            print(product.tittle)
+        }.disposed(by: bag)
+        //fetch item
+        viewModel.fetchItem()
+    }
 
 }
 
